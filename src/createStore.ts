c83deba4d7022b80b68cadddf47750b10c5700b0
Bartłomiej Bykowy <bartlomiej.bykowy@gmail.com>
2025-11-30 21:39:49 +0100

    Implemenet createStore function

--- src/createStore.ts
+++ src/createStore.ts
@@ -11,5 +11,33 @@ export function createStore<
   G extends Getters<S>,
   A extends Actions
 >(config: StoreConfig<S, G, A>): StoreInstance<S, G, A> {
-  // TODO: implement function
+  const store = {} as StoreInstance<S, G, A>;
+
+  const { id, state, getters, actions } = config;
+
+  store.$id = id;
+  store.$state = state();
+
+  // ADD GETTERS AS COMPUTED VALUES
+  if (getters && Object.keys(getters).length) {
+    for (const key in getters) {
+      const getter = getters[key];
+      (store as any)[key] = getter(store.$state, store);
+    }
+  }
+  // ADD ACTIONS
+  if (actions && Object.keys(actions).length) {
+    for (const key in actions) {
+      const action = actions[key];
+      (store as any)[key] = action.bind(store);
+    }
+  }
+  // ADD STORE METHODS
+  // TODO: implement methods
+  store.$patch = () => console.log("path");
+  store.$reset = () => console.log("reset");
+  store.$subscribe = (cb) => () => console.log("path");
+  store.$subscribeKey = (key, cb) => () => console.log("path");
+
+  return store;
 }
