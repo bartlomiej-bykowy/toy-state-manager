2a026e26814597f107f9834f5d51871f868bbfb8
Bartłomiej Bykowy <bartlomiej.bykowy@gmail.com>
2025-12-02 22:02:12 +0100

    Implement $reset method

--- src/createStore.ts
+++ src/createStore.ts
@@ -5,18 +5,22 @@ import type {
   StoreConfig,
   StoreInstance
 } from "./types";
+import { cloneDeep } from "./utils";
 
 export function createStore<
   S extends State,
   G extends Getters<S>,
   A extends Actions
 >(config: StoreConfig<S, G, A>): StoreInstance<S, G, A> {
-  const store = {} as StoreInstance<S, G, A>;
-
   const { id, state, getters, actions } = config;
 
+  const rawState = state();
+  const initialState = cloneDeep(rawState);
+
+  const store = {} as StoreInstance<S, G, A>;
+
   store.$id = id;
-  store.$state = state();
+  store.$state = initialState;
 
   // ADD GETTERS AS COMPUTED VALUES
   if (getters && Object.keys(getters).length) {
@@ -34,8 +38,27 @@ export function createStore<
   }
   // ADD STORE METHODS
   // TODO: implement methods
-  store.$patch = () => console.log("path");
-  store.$reset = () => console.log("reset");
+  store.$patch = (partialState: Partial<S>) => {
+    for (const key in partialState) {
+      if (!(key in store.$state)) {
+        throw new Error(`State does not include property "${key}".`);
+      }
+
+      store.$state[key as keyof S] = partialState[key as keyof S]!;
+    }
+  };
+  store.$reset = (...args: (keyof S)[]) => {
+    if (args.length === 0) {
+      for (const key in initialState) {
+        store.$state[key] = cloneDeep(initialState[key]);
+      }
+      return;
+    }
+
+    for (const key of args) {
+      store.$state[key] = cloneDeep(initialState[key]);
+    }
+  };
   store.$subscribe = (cb) => () => console.log("path");
   store.$subscribeKey = (key, cb) => () => console.log("path");
 
